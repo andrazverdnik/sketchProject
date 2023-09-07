ee6f52d2472b37a902aa668cf4d60133997d8f9f
Andraž Verdnik <grillbottoms@Andrazs-MacBook-Pro.local>
2023-09-07 20:14:42 +0200

    create a grid, add simple drawing and resizing

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ const rainbow = document.getElementById("rainbow")
 const canvasSize = document.getElementById("canvasSize")
 const canvas = document.getElementById("canvas")
 
+
+
 function animateButton(element, time = 250){
     element.addEventListener("click", ()=> {
+        console.log("Animating button")
         element.classList.remove("toolbarButton")
         element.classList.add("toolbarButtonPressed")
         setTimeout(()=> {
@@ -15,8 +18,45 @@ function animateButton(element, time = 250){
     })
 }
 
+function pixelHover(element){
+    element.addEventListener("mouseover", ()=>{
+        element.style.backgroundColor = "black";
+    })
+}
+
+function createGrid(length){
+    size = length*length
+    if(length<100){
+        for(let i = 0; i<size; i++){
+            console.log("Creating a \"pixel\"")
+            pixel = document.createElement("div")
+            pixel.classList.add("pixel")
+            pixelHover(pixel)
+            canvas.append(pixel)
+        }
+        canvas.style.gridTemplateColumns = `repeat(${length}, 1fr)`;
+        canvas.style.gridTemplateRows = `repeat(${length}, 1fr)`;
+    } else {
+        alert("Nice try")
+    }
+    }
+    
+
+function destroyGrid(){
+    canvas.innerHTML = ''
+}
+
+
+
+canvasSize.addEventListener("click", ()=> {
+    destroyGrid()
+    length = parseInt(prompt("What should the width and height be? Input only 1 number, it will be a square."))
+    createGrid(length)
+})
+
 animateButton(pickColor)
 animateButton(eraser)
 animateButton(rainbow)
 animateButton(canvasSize)
+createGrid(16)
 
